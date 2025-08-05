f4e725fad0ee838f9a4a6edbcc1c0f600075dd6d
Peter Berck <peter.berck@humlab.lu.se>
2025-08-05 13:36:29 +0200

    Slider added.

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,65 @@
 use iced::{
-    widget::{button, column, text},
-    Element,
+    widget::{button, column, row, slider, text},
+    Element, Task,
 };
 
 #[derive(Debug, Default)]
-struct AppState {}
+struct AppState {
+    ph_diameter: f32,
+    ph_thickness: f32,
+    ph_viewangle: f32,
+}
 
 #[derive(Debug, Clone)]
 enum Message {
     Exit,
+    UpdatePhDiameter(f32),
 }
 
 impl AppState {
-    fn update(state: &mut AppState, message: Message) {
+    fn new() -> (Self, Task<Message>) {
+        (
+            Self {
+                ph_diameter: 0.05,
+                ph_thickness: 0.1,
+                ph_viewangle: 0.,
+            },
+            Task::none(),
+        )
+    }
+
+    fn update(&mut self, message: Message) {
         match message {
             Message::Exit => eprintln!("Exit"),
+            Message::UpdatePhDiameter(v) => self.ph_diameter = v,
         }
     }
 
-    fn view(state: &AppState) -> Element<Message> {
-        column![text("PHCalc"), button("Exit").on_press(Message::Exit),].into()
+    fn view(&self) -> Element<Message> {
+        column![
+            text("PHCalc").size(32),
+            column![
+                text("View angle").size(32),
+                row![
+                    text(self.ph_diameter),
+                    slider(0.01..=1.00, self.ph_diameter, |v| {
+                        Message::UpdatePhDiameter(v)
+                    })
+                    .step(0.01),
+                ],
+                text(self.ph_viewangle),
+            ],
+            column![text("Focal length").size(32),],
+            button(text("Exit").size(24))
+                .padding(8)
+                .on_press(Message::Exit),
+        ]
+        .into()
     }
 }
 
 fn main() -> iced::Result {
     iced::application("PHCalc", AppState::update, AppState::view)
-        //.theme(|_| iced::Theme::KanagawaDragon)
-        .run()
+        .theme(|_| iced::Theme::KanagawaDragon)
+        .run_with(AppState::new)
 }
